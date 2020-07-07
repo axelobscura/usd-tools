5a23c9469bf15903a64664c063c3f9cd39646a2c
Axel Obscura <axosar@gmail.com>
2020-07-07 16:53:59 -0500

    Estilos menu lateral

--- src/components/MenuLateral.js
+++ src/components/MenuLateral.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import './MenuLateral.css';
+
 function MenuLateral() {
     return (
         <>
-            <ul>
+            <ul className="MenuLateral">
                 <li><h3>SECCIÓN I</h3>
                     <ul>
                         <li><Link><b>Reporte 1</b><br />Validación de Estructura  del Reporte</Link></li>
