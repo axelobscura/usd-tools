232627ebc070ac848a96abdeec973f27b860f62a
Axel Obscura <axosar@gmail.com>
2020-07-10 12:43:29 -0500

    Barras

--- src/components/dashboards/Seccion1Reporte2.js
+++ src/components/dashboards/Seccion1Reporte2.js
@@ -6,6 +6,7 @@ import Titulo from './Titulo/Titulo';
 import Form from 'react-bootstrap/Form';
 import Dona from './Graficas/Dona';
 import Linear from './Graficas/Linear';
+import Barras from './Graficas/Barras';
 import Tabla from './Graficas/Tabla';
 
 function Seccion1Reporte2() {
@@ -28,20 +29,31 @@ function Seccion1Reporte2() {
                 </Row>
                 <Row>
                     <Col>
-                        <h4><span class="lnr lnr-chevron-right"></span> Número de Errores de Estructura por etiqueta en reporte</h4>
+                        <h4><span class="lnr lnr-chevron-right"></span> Número de Errores de contenido en etiquetas por reporte</h4>
                         <hr />
                         <Dona />
                     </Col>
                     <Col>
-                        <h4><span class="lnr lnr-chevron-right"></span> Tendencia de errores de estructura por etiqueta</h4>
+                        <h4><span class="lnr lnr-chevron-right"></span> Tendencia de errores de contenido por etiqueta</h4>
                         <hr />
                         <Linear />
+                    </Col>
+                    <Col>
+                        <h4><span class="lnr lnr-chevron-right"></span> Errores en etiquetas por mal uso de catálogos</h4>
+                        <hr />
+                        <Dona />
                     </Col>
                 </Row>
                 <Row>
                     <Col>
                         <hr />
-                        <h4><span class="lnr lnr-chevron-right"></span> Errores de estructura por etiqueta en reporte</h4>
+                        <h4><span class="lnr lnr-chevron-right"></span> Tipos de errores encontrados por cada etiqueta</h4>
+                        <hr />
+                        <Barras />
+                    </Col>
+                    <Col>
+                        <hr />
+                        <h4><span class="lnr lnr-chevron-right"></span> Campos erroneos por reporte</h4>
                         <hr />
                         <Tabla />
                     </Col>
