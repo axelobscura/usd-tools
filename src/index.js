27883ee84ff244650cd59c92742f4eac2de6a775
Axel Obscura <axosar@gmail.com>
2020-07-10 10:54:21 -0500

    Router2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>,
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
