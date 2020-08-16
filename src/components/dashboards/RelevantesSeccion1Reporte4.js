12819a99a66d5bf2e14fe4441af7f5ddaa9883f6
axelobscura <axosar@gmail.com>
2020-08-16 14:33:41 -0500

    Tabla dof

--- src/components/dashboards/RelevantesSeccion1Reporte4.js
+++ src/components/dashboards/RelevantesSeccion1Reporte4.js
@@ -4,8 +4,11 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Titulo from './Titulo/Titulo';
+import Tabla from './Graficas/Tabla';
 
 function RelevantesSeccion1Reporte4() {
+    const tablaHeader1 = ["Fecha","Tipo de cambio publicado por el DOF"];
+    const tablaContenido1 = ["20190101", "19,28"];
     return (
         <>
             <Titulo texto="Reporte 4: Validación de Tipo de Cambio" />
@@ -14,13 +17,23 @@ function RelevantesSeccion1Reporte4() {
                     <Col>
                         <Form.Group style={{ width: '100%' }}>
                             <Form.Control as="select" size="lg" custom>
-                                <option>Seleccionar periodo</option>
+                                <option>Seleccionar periodo de consulta DOF</option>
                                 <option>2</option>
                                 <option>3</option>
                                 <option>4</option>
                                 <option>5</option>
                             </Form.Control>
                         </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Tabla
+                            tablaHeader={[...tablaHeader1]}
+                            contenido={tablaContenido1}
+                        />
+                    </Col>
+                    <Col>
                     </Col>
                 </Row>
             </Container>
