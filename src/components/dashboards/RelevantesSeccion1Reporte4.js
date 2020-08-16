6e9e7d3b034cdf864ebd8f09dd6993b91d78f7a8
axelobscura <axosar@gmail.com>
2020-08-16 14:29:20 -0500

    Tabla Reporte 4

--- src/components/dashboards/RelevantesSeccion1Reporte4.js
+++ src/components/dashboards/RelevantesSeccion1Reporte4.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Titulo from './Titulo/Titulo';
@@ -11,7 +12,15 @@ function RelevantesSeccion1Reporte4() {
             <Container fluid={true}>
                 <Row>
                     <Col>
-                        
+                        <Form.Group style={{ width: '100%' }}>
+                            <Form.Control as="select" size="lg" custom>
+                                <option>Seleccionar periodo</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Form.Control>
+                        </Form.Group>
                     </Col>
                 </Row>
             </Container>
